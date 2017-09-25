d67d56545a4839017f229e004c9fd3a048b08039
chhaichivon <chhaichivon1995@gmail.com>
2017-09-25 13:12:03 +0700

    CRUD product

--- frontend/src/main/spring-boot-ng2/src/app/api/api.ts
+++ frontend/src/main/spring-boot-ng2/src/app/api/api.ts
@@ -1,3 +1,17 @@
 
-export const webServiceEndPoint: String = 'http://localhost:8080/api/';
+export const API_END_POINT : String = 'http://localhost:8080/api/';
 
+export const HEADER = {
+  headers:{
+    'Content-Type':'application/json'
+  }
+};
+
+export const AUTH_CONFIG = (token) => {
+  return {
+    header:{
+      'Content-Type':'application/json',
+      'Authorization':'Bearer '+ token
+    }
+  }
+};
--- frontend/src/main/spring-boot-ng2/src/app/pages/home/home.component.ts
+++ frontend/src/main/spring-boot-ng2/src/app/pages/home/home.component.ts
@@ -2,6 +2,9 @@ import 'rxjs/add/operator/map';
 import { Http } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
 
+
+import { API_END_POINT } from './../../api/api';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   public categories;
+  public products;
 
   constructor(private http: Http) {
 
@@ -23,6 +27,13 @@ export class HomeComponent implements OnInit {
 
       console.log(res.DATA);
     });
+
+
+    this.http.get('http://localhost:9000/api/products')
+      .map(res => res.json())
+      .subscribe(res => {
+        this.products = res.DATA
+      })
   }
 
 }
